c9b561b8b07b5f2527e2bad9f130df1fcf39ed1a
Leonid Kurevin <jokout321@gmail.com>
2022-11-27 15:35:35 +0300

    add step 7

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,6 +8,8 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 const casesWithFormat = [
+  ['file1.yml', 'file2.yml', 'result_plain.txt', 'plain'],
+  ['file1.json', 'file2.json', 'result_plain.txt', 'plain'],
   ['file1.json', 'file2.json', 'result_json.txt', 'stylish'],
   ['file1.yml', 'file2.yml', 'result_json.txt', 'stylish'],
 ];
